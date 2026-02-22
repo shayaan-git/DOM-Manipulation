87c7e7898cd54ebb257a77e584034d31212aae5c
shayaan-git <mshayaan24@gmail.com>
2026-02-23 00:56:18 +0530

    color changing screen mini task in DOM

---
+++ Dom_lec-2(Task)/scriptd2.js
@@ -0,0 +1,14 @@
+// main, box, btn
+
+var box = document.querySelector("#box");
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  var c1 = Math.floor(Math.random() * 256);
+  var c2 = Math.floor(Math.random() * 256);
+  var c3 = Math.floor(Math.random() * 256);
+
+  //   console.log(c1, c2, c3);
+  box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+  box.innerHTML = `rgb(${c1}, ${c2}, ${c3})`;
+});
